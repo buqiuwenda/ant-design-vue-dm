5454e8f1527467b060eeaaa6f4df70075db49f60
wenda <765196580@qq.com>
2020-09-01 09:46:22 +0800

    drawer

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,32 +1,36 @@
 <template>
-  <a-layout id="components-layout-demo-side" style="min-height: 100vh">
-    <a-layout-sider :trigger="null" v-model="collapsed" collapsible>
-      <div class="logo" />
-      <Sider></Sider>
-    </a-layout-sider>
-    <a-layout>
-      <a-layout-header style="background: #fff; padding: 0">
-        <a-icon
-          class="trigger"
-          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
-          @click="collapsed != collapsed"
-        >
-        </a-icon>
-        <Header></Header>
-      </a-layout-header>
-      <a-layout-content style="margin: 0 16px">
-        <router-view></router-view>
-      </a-layout-content>
-      <a-layout-footer style="text-align: center">
-        <Footer></Footer>
-      </a-layout-footer>
+  <div>
+    <a-layout id="components-layout-demo-side" style="min-height: 100vh">
+      <a-layout-sider :trigger="null" v-model="collapsed" collapsible>
+        <div class="logo" />
+        <Sider></Sider>
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header style="background: #fff; padding: 0">
+          <a-icon
+            class="trigger"
+            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+            @click="collapsed != collapsed"
+          >
+          </a-icon>
+          <Header></Header>
+        </a-layout-header>
+        <a-layout-content style="margin: 0 16px">
+          <router-view></router-view>
+        </a-layout-content>
+        <a-layout-footer style="text-align: center">
+          <Footer></Footer>
+        </a-layout-footer>
+      </a-layout>
     </a-layout>
-  </a-layout>
+    <SettingDrawer />
+  </div>
 </template>
 <script>
 import Header from "./Header.vue";
 import Footer from "./Footer.vue";
 import Sider from "./SiderMenu.vue";
+import SettingDrawer from "../components/SettingDrawer";
 export default {
   data() {
     return {
@@ -36,7 +40,8 @@ export default {
   components: {
     Header,
     Footer,
-    Sider
+    Sider,
+    SettingDrawer
   }
 };
 </script>
