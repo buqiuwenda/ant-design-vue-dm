1632384112ccf5757b35fbe17e4e63db89a01190
wenda <765196580@qq.com>
2020-09-03 09:28:52 +0800

    menu save

--- src/layouts/SiderMenu.vue
+++ src/layouts/SiderMenu.vue
@@ -1,7 +1,40 @@
 <template>
-  <div>Sider</div>
+  <div style="width: 256px">
+    <a-menu
+      :default-selected-keys="['1']"
+      :default-open-keys="['2']"
+      mode="inline"
+      theme="dark"
+      :inline-collapsed="collapsed"
+    >
+      <template v-for="item in list">
+        <a-menu-item v-if="!item.children" :key="item.key">
+          <a-icon type="pie-chart" />
+          <span>{{ item.title }}</span>
+        </a-menu-item>
+        <sub-menu v-else :key="item.key" :menu-info="item" />
+      </template>
+    </a-menu>
+  </div>
 </template>
+
 <script>
-export default {};
+import SubMenu from "./SubMenu";
+
+export default {
+  components: {
+    "sub-menu": SubMenu
+  },
+  data() {
+    return {
+      collapsed: false,
+      list: []
+    };
+  },
+  methods: {
+    toggleCollapsed() {
+      this.collapsed = !this.collapsed;
+    }
+  }
+};
 </script>
-<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/user",
+    hideInMenu: true,
     component: () =>
       import(/* webpackChunkName: "layouts" */ "../layouts/UserLayout.vue"),
     children: [
