8f7e93b4d800cab99fa619b3af52b1c4b12e4cfa
wenda <765196580@qq.com>
2020-09-09 09:29:22 +0800

    menu

--- src/layouts/SiderMenu.vue
+++ src/layouts/SiderMenu.vue
@@ -7,12 +7,12 @@
       theme="dark"
       :inline-collapsed="collapsed"
     >
-      <template v-for="item in list">
-        <a-menu-item v-if="!item.children" :key="item.key">
-          <a-icon type="pie-chart" />
-          <span>{{ item.title }}</span>
+      <template v-for="item in menuData">
+        <a-menu-item v-if="!item.children" :key="item.path">
+          <a-icon v-if="item.meta.icon" :type="item.meta.icon" />
+          <span>{{ item.meta.title }}</span>
         </a-menu-item>
-        <sub-menu v-else :key="item.key" :menu-info="item" />
+        <sub-menu v-else :key="item.path" :menu-info="item" />
       </template>
     </a-menu>
   </div>
@@ -26,14 +26,37 @@ export default {
     "sub-menu": SubMenu
   },
   data() {
+    const menuData = this.getMenuData(this.$routes.options.routes);
     return {
-      collapsed: false,
-      list: []
+      menuData
     };
   },
   methods: {
     toggleCollapsed() {
       this.collapsed = !this.collapsed;
+    },
+    getMenuData(routes = []) {
+      const menuData = [];
+
+      for (let item of routes) {
+        if (item.name && !item.hideItem) {
+          const newItem = { ...item };
+          delete newItem.children;
+          if (item.children && !item.hideChildrenInMenu) {
+            newItem.children = this.getMenuData(item.children);
+          } else {
+            this.getMenuData(item.children);
+          }
+          menuData.push(newItem);
+        } else if (
+          !item.hideItem &&
+          !item.hideChildrenInMenu &&
+          item.children
+        ) {
+          menuData.push(...this.getMenuData(item.children));
+        }
+      }
+      return menuData;
     }
   }
 };
